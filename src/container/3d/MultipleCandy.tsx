54b5c1c27fcda179325e60fcb6b33d4e3815bea1
AALIYAH FARAH ADIBAH <aaliyahfarahadibah@gmail.com>
2022-12-13 00:23:23 +0700

    feat: add each candy type animation

--- src/container/3d/MultipleCandy.tsx
+++ src/container/3d/MultipleCandy.tsx
@@ -6,7 +6,17 @@ export default function MultipleCandy() {
   return (
     //!TODO Aaliyah
     <React.Fragment>
+      {/* Chupa-chupz */}
       <SingleCandy name='chupa-cupz' position={[-0.34, 0.82, -0.15]} />
+
+      {/* Dalgona */}
+      <SingleCandy name='dalgona' position={[0.93, 0.72, -0.15]} />
+
+      {/* Mentoz */}
+      <SingleCandy name='mentoz' position={[-1.05, 0.785, -0.15]} />
+
+      {/* Suguz */}
+      <SingleCandy name='suguz' position={[0.3, 0.79, -0.15]} />
     </React.Fragment>
   );
 }
