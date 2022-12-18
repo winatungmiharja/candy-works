9219c08570516f707d59bb0fbaee416bab8217d9
silabanjames <silaban.james13@gmail.com>
2022-12-19 01:20:39 +0700

    trying solve prettier issue

--- src/data/candy.tsx
+++ src/data/candy.tsx
@@ -5,33 +5,24 @@ export const CandyData: { [key in keyof typeof CandyType]: CandyDataType } = {
     url: 'mentoz',
     name: 'Mentoz',
     sold: 100,
-    description: 'Candy with a strong mint flavor provides freshness all day long',
+    description:
+      'Candy with a strong mint flavor provides freshness all day long',
     nutrition: (
       <>
         <p>
-          Total Calories 10 kcal,
-          Calories from Fat 0 kcal,
-          Total Fat 0 g,
-          Saturated Fat 0 g,
-          Sodium 0 mg,
-          Total Carbohydrates 3 g,
-          Sugars 2 g,
-          Protein 0 g</p>
+          Total Calories 10 kcal, Calories from Fat 0 kcal, Total Fat 0 g,
+          Saturated Fat 0 g, Sodium 0 mg, Total Carbohydrates 3 g, Sugars 2 g,
+          Protein 0 g
+        </p>
       </>
     ),
     ingredients: (
       <>
         <p>
-          Sugar,
-          Wheat Glucose Syrup,
-          Hydrogenated Coconut Oil,
-          Rice Starch,
-          Natural Flavors,
-          Gum Arabic,
-          Sucrose Esters Of Fatty Acids,
-          Gellan Gum,
-          Carnauba Wax,
-          Beeswax</p>
+          Sugar, Wheat Glucose Syrup, Hydrogenated Coconut Oil, Rice Starch,
+          Natural Flavors, Gum Arabic, Sucrose Esters Of Fatty Acids, Gellan
+          Gum, Carnauba Wax, Beeswax
+        </p>
       </>
     ),
   },
@@ -43,13 +34,8 @@ export const CandyData: { [key in keyof typeof CandyType]: CandyDataType } = {
     nutrition: (
       <>
         <p>
-          Total Calories 0 kcal,
-          Calories from fat 0 kcal,
-          Total Fat 0 g,
-          Saturated Fat 0 g,
-          Protein 0 g,
-          Total Carbohydrate 2 g,
-          Sugars 2 g,
+          Total Calories 0 kcal, Calories from fat 0 kcal, Total Fat 0 g,
+          Saturated Fat 0 g, Protein 0 g, Total Carbohydrate 2 g, Sugars 2 g,
           Salt (Sodium) 0 mg
         </p>
       </>
@@ -57,17 +43,9 @@ export const CandyData: { [key in keyof typeof CandyType]: CandyDataType } = {
     ingredients: (
       <>
         <p>
-          Sugar,
-          Glucose,
-          Vegetable Fat,
-          Gelling Agent,
-          Synthetic Flavours,
-          Salt,
-          Natural Flavour,
-          Vegetable Emulsifier,
-          Natural Colour,
-          Antioxidant,
-          Synthetic Colours
+          Sugar, Glucose, Vegetable Fat, Gelling Agent, Synthetic Flavours,
+          Salt, Natural Flavour, Vegetable Emulsifier, Natural Colour,
+          Antioxidant, Synthetic Colours
         </p>
       </>
     ),
@@ -80,21 +58,14 @@ export const CandyData: { [key in keyof typeof CandyType]: CandyDataType } = {
     nutrition: (
       <>
         <p>
-          Total Calories 374 kcal,
-          Protein 14 g,
-          Fat 9 g,
-          Total Carbohydrate 60 g
+          Total Calories 374 kcal, Protein 14 g, Fat 9 g, Total Carbohydrate 60
+          g
         </p>
       </>
     ),
     ingredients: (
       <>
-        <p>
-          Coffee powder,
-          Sugars,
-          Milk,
-          Baking Soda
-        </p>
+        <p>Coffee powder, Sugars, Milk, Baking Soda</p>
       </>
     ),
   },
@@ -107,13 +78,8 @@ export const CandyData: { [key in keyof typeof CandyType]: CandyDataType } = {
     nutrition: (
       <>
         <p>
-          Total Calories 30 kcal,
-          Calories from Fat 0 kcal,
-          Total Fat 0 g,
-          Saturated Fat 0 g,
-          Protein 0 g,
-          Total Carbohydrates 7 g,
-          Sugar 3 g,
+          Total Calories 30 kcal, Calories from Fat 0 kcal, Total Fat 0 g,
+          Saturated Fat 0 g, Protein 0 g, Total Carbohydrates 7 g, Sugar 3 g,
           Natrium 10 mg
         </p>
       </>
@@ -121,14 +87,9 @@ export const CandyData: { [key in keyof typeof CandyType]: CandyDataType } = {
     ingredients: (
       <>
         <p>
-          Glucose Syrup,
-          Sugars,
-          Wheat Flour,
-          Invert Sugar,
-          Acidity Regulator,
-          Hydrogenated Coconut Oil,
-          Flavour,
-          Colours (Tartazine, Allura Red AC, Briliant Blue FCF, Sunset Yellow, CFC)
+          Glucose Syrup, Sugars, Wheat Flour, Invert Sugar, Acidity Regulator,
+          Hydrogenated Coconut Oil, Flavour, Colours (Tartazine, Allura Red AC,
+          Briliant Blue FCF, Sunset Yellow, CFC)
         </p>
       </>
     ),
