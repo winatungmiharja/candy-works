49824e10b330813facb18c5d22f7d7290e008f55
unknown <silabanjames>
2022-12-18 23:38:04 +0700

    done description, ingredients, and nutrition

--- src/data/candy.tsx
+++ src/data/candy.tsx
@@ -5,15 +5,33 @@ export const CandyData: { [key in keyof typeof CandyType]: CandyDataType } = {
     url: 'mentoz',
     name: 'Mentoz',
     sold: 100,
-    description: 'lorem ipsum',
+    description: 'Candy with a strong mint flavor provides freshness all day long',
     nutrition: (
       <>
-        <p>lala</p>
+        <p>
+          Total Calories 10 kcal,
+          Calories from Fat 0 kcal,
+          Total Fat 0 g,
+          Saturated Fat 0 g,
+          Sodium 0 mg,
+          Total Carbohydrates 3 g,
+          Sugars 2 g,
+          Protein 0 g</p>
       </>
     ),
     ingredients: (
       <>
-        <p>lala</p>
+        <p>
+          Sugar,
+          Wheat Glucose Syrup,
+          Hydrogenated Coconut Oil,
+          Rice Starch,
+          Natural Flavors,
+          Gum Arabic,
+          Sucrose Esters Of Fatty Acids,
+          Gellan Gum,
+          Carnauba Wax,
+          Beeswax</p>
       </>
     ),
   },
@@ -21,15 +39,36 @@ export const CandyData: { [key in keyof typeof CandyType]: CandyDataType } = {
     url: 'suguz',
     name: 'Suguz',
     sold: 100,
-    description: 'lorem ipsum',
+    description: 'Sweet candy that spoils your mouth',
     nutrition: (
       <>
-        <p>lala</p>
+        <p>
+          Total Calories 0 kcal,
+          Calories from fat 0 kcal,
+          Total Fat 0 g,
+          Saturated Fat 0 g,
+          Protein 0 g,
+          Total Carbohydrate 2 g,
+          Sugars 2 g,
+          Salt (Sodium) 0 mg
+        </p>
       </>
     ),
     ingredients: (
       <>
-        <p>lala</p>
+        <p>
+          Sugar,
+          Glucose,
+          Vegetable Fat,
+          Gelling Agent,
+          Synthetic Flavours,
+          Salt,
+          Natural Flavour,
+          Vegetable Emulsifier,
+          Natural Colour,
+          Antioxidant,
+          Synthetic Colours
+        </p>
       </>
     ),
   },
@@ -37,15 +76,25 @@ export const CandyData: { [key in keyof typeof CandyType]: CandyDataType } = {
     url: 'dalgona',
     name: 'Dalgona',
     sold: 100,
-    description: 'lorem ipsum',
+    description: 'Boost your energy with Dalgona ',
     nutrition: (
       <>
-        <p>lala</p>
+        <p>
+          Total Calories 374 kcal,
+          Protein 14 g,
+          Fat 9 g,
+          Total Carbohydrate 60 g
+        </p>
       </>
     ),
     ingredients: (
       <>
-        <p>lala</p>
+        <p>
+          Coffee powder,
+          Sugars,
+          Milk,
+          Baking Soda
+        </p>
       </>
     ),
   },
@@ -54,15 +103,33 @@ export const CandyData: { [key in keyof typeof CandyType]: CandyDataType } = {
     url: 'chupa-cupz',
     name: 'Chupa Cupz',
     sold: 100,
-    description: 'lorem ipsum',
+    description: 'Round candy that never runs out when sucked',
     nutrition: (
       <>
-        <p>lala</p>
+        <p>
+          Total Calories 30 kcal,
+          Calories from Fat 0 kcal,
+          Total Fat 0 g,
+          Saturated Fat 0 g,
+          Protein 0 g,
+          Total Carbohydrates 7 g,
+          Sugar 3 g,
+          Natrium 10 mg
+        </p>
       </>
     ),
     ingredients: (
       <>
-        <p>lala</p>
+        <p>
+          Glucose Syrup,
+          Sugars,
+          Wheat Flour,
+          Invert Sugar,
+          Acidity Regulator,
+          Hydrogenated Coconut Oil,
+          Flavour,
+          Colours (Tartazine, Allura Red AC, Briliant Blue FCF, Sunset Yellow, CFC)
+        </p>
       </>
     ),
   },
