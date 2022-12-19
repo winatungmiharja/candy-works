9e7bb3338cf26c02a6f1c13b4bdb4f3b3d431799
winatungmiharja <winatungmiharja@gmail.com>
2022-12-19 13:36:13 +0700

    fix: ssr hydration error

--- src/data/candy.tsx
+++ src/data/candy.tsx
@@ -1,5 +1,5 @@
 import { CandyDataType, CandyType } from '@/types/candy.';
-//!TODO James
+
 export const CandyData: { [key in keyof typeof CandyType]: CandyDataType } = {
   mentoz: {
     url: 'mentoz',
@@ -9,20 +9,16 @@ export const CandyData: { [key in keyof typeof CandyType]: CandyDataType } = {
       'Candy with a strong mint flavor provides freshness all day long',
     nutrition: (
       <>
-        <p>
-          Total Calories 10 kcal, Calories from Fat 0 kcal, Total Fat 0 g,
-          Saturated Fat 0 g, Sodium 0 mg, Total Carbohydrates 3 g, Sugars 2 g,
-          Protein 0 g
-        </p>
+        Total Calories 10 kcal, Calories from Fat 0 kcal, Total Fat 0 g,
+        Saturated Fat 0 g, Sodium 0 mg, Total Carbohydrates 3 g, Sugars 2 g,
+        Protein 0 g
       </>
     ),
     ingredients: (
       <>
-        <p>
-          Sugar, Wheat Glucose Syrup, Hydrogenated Coconut Oil, Rice Starch,
-          Natural Flavors, Gum Arabic, Sucrose Esters Of Fatty Acids, Gellan
-          Gum, Carnauba Wax, Beeswax
-        </p>
+        Sugar, Wheat Glucose Syrup, Hydrogenated Coconut Oil, Rice Starch,
+        Natural Flavors, Gum Arabic, Sucrose Esters Of Fatty Acids, Gellan Gum,
+        Carnauba Wax, Beeswax
       </>
     ),
   },
@@ -33,20 +29,16 @@ export const CandyData: { [key in keyof typeof CandyType]: CandyDataType } = {
     description: 'Sweet candy that spoils your mouth',
     nutrition: (
       <>
-        <p>
-          Total Calories 0 kcal, Calories from fat 0 kcal, Total Fat 0 g,
-          Saturated Fat 0 g, Protein 0 g, Total Carbohydrate 2 g, Sugars 2 g,
-          Salt (Sodium) 0 mg
-        </p>
+        Total Calories 0 kcal, Calories from fat 0 kcal, Total Fat 0 g,
+        Saturated Fat 0 g, Protein 0 g, Total Carbohydrate 2 g, Sugars 2 g, Salt
+        (Sodium) 0 mg
       </>
     ),
     ingredients: (
       <>
-        <p>
-          Sugar, Glucose, Vegetable Fat, Gelling Agent, Synthetic Flavours,
-          Salt, Natural Flavour, Vegetable Emulsifier, Natural Colour,
-          Antioxidant, Synthetic Colours
-        </p>
+        Sugar, Glucose, Vegetable Fat, Gelling Agent, Synthetic Flavours, Salt,
+        Natural Flavour, Vegetable Emulsifier, Natural Colour, Antioxidant,
+        Synthetic Colours
       </>
     ),
   },
@@ -57,17 +49,10 @@ export const CandyData: { [key in keyof typeof CandyType]: CandyDataType } = {
     description: 'Boost your energy with Dalgona ',
     nutrition: (
       <>
-        <p>
-          Total Calories 374 kcal, Protein 14 g, Fat 9 g, Total Carbohydrate 60
-          g
-        </p>
+        Total Calories 374 kcal, Protein 14 g, Fat 9 g, Total Carbohydrate 60 g
       </>
     ),
-    ingredients: (
-      <>
-        <p>Coffee powder, Sugars, Milk, Baking Soda</p>
-      </>
-    ),
+    ingredients: <>Coffee powder, Sugars, Milk, Baking Soda</>,
   },
 
   'chupa-cupz': {
@@ -77,20 +62,16 @@ export const CandyData: { [key in keyof typeof CandyType]: CandyDataType } = {
     description: 'Round candy that never runs out when sucked',
     nutrition: (
       <>
-        <p>
-          Total Calories 30 kcal, Calories from Fat 0 kcal, Total Fat 0 g,
-          Saturated Fat 0 g, Protein 0 g, Total Carbohydrates 7 g, Sugar 3 g,
-          Natrium 10 mg
-        </p>
+        Total Calories 30 kcal, Calories from Fat 0 kcal, Total Fat 0 g,
+        Saturated Fat 0 g, Protein 0 g, Total Carbohydrates 7 g, Sugar 3 g,
+        Natrium 10 mg
       </>
     ),
     ingredients: (
       <>
-        <p>
-          Glucose Syrup, Sugars, Wheat Flour, Invert Sugar, Acidity Regulator,
-          Hydrogenated Coconut Oil, Flavour, Colours (Tartazine, Allura Red AC,
-          Briliant Blue FCF, Sunset Yellow, CFC)
-        </p>
+        Glucose Syrup, Sugars, Wheat Flour, Invert Sugar, Acidity Regulator,
+        Hydrogenated Coconut Oil, Flavour, Colours (Tartazine, Allura Red AC,
+        Briliant Blue FCF, Sunset Yellow, CFC)
       </>
     ),
   },
